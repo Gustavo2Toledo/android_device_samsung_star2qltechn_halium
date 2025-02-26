a94ca91a697eaff262f63b7bcfcd632b5115f028
Gustavo2Toledo <108426956+Gustavo2Toledo@users.noreply.github.com>
2025-02-26 20:42:37 -0300

    Update lineage_star2qltechn.mk

--- samsung/star2qltechn/lineage_star2qltechn.mk
+++ samsung/star2qltechn/lineage_star2qltechn.mk
@@ -3,9 +3,5 @@ $(call inherit-product, device/samsung/star2qltechn/full_star2qltechn.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_star2qltechn
+PRODUCT_NAME := halium_star2qltechn
 BOARD_VENDOR := samsung
-
-# OTA
-PRODUCT_PROPERTY_OVERRIDES += \
-	lineage.updater.uri=https://raw.githubusercontent.com/klabit87/OTA/master/star2qltechn.json
