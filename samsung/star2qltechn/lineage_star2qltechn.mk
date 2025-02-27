505b65c278fb719637cd12ff7b0d149ec24ad6fe
Gustavo2Toledo <108426956+Gustavo2Toledo@users.noreply.github.com>
2025-02-26 21:14:37 -0300

    Delete samsung/star2qltechn directory

--- samsung/star2qltechn/lineage_star2qltechn.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, device/samsung/star2qltechn/full_star2qltechn.mk)
-
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := halium_star2qltechn
-BOARD_VENDOR := samsung
